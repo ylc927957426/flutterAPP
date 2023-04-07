1349c485dbaaa73a94dda807673b4edf81fc8956
于龙传 <yulch@toplion.com.cn>
2023-04-07 16:03:27 +0800

    搭建首页

--- lib/home/index.dart
+++ lib/home/index.dart
@@ -1,85 +1,46 @@
 import 'package:flutter/material.dart';
+import '../text/index.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List _tabs = [
+    {
+      'url': const MyText(),
+      'label': '首页',
+      'bottomLabel': '首页顶部',
+      'icon': const Icon(Icons.home),
+    },
+  ];
+  int _current = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('首页'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '欢迎来到app，字体对齐方式',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              "使用Text标签的*4 操作，最大一行，超出隐藏 " * 4,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const Text(
-              '改变字体大小',
-              textScaleFactor: 1.5,
-            ),
-            Text(
-              '修改字体样式Style',
-              style: TextStyle(
-                  color: Colors.pink,
-                  fontSize: 20,
-                  fontFamily: "Courier",
-                  background: Paint()..color = Colors.amber,
-                  decorationStyle: TextDecorationStyle.wavy,
-                  decoration: TextDecoration.underline),
-            ),
-            const Text.rich(TextSpan(children: [
-              TextSpan(text: '百度链接：'),
-              TextSpan(
-                text: "http://www.baidu.com",
-                style: TextStyle(color: Colors.blue),
-              ),
-            ])),
-            ElevatedButton(
-              child: const Text("点击跳转商品页面"),
-              onPressed: () async {
-                var result = await Navigator.pushNamed(context, '/goods',
-                    arguments: {'goodsName': '商品name'});
-                print(
-                  '从商品页面返回带来的参数: $result',
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.amber,
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                },
-                icon: const Icon(Icons.home)),
-            IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/goods');
-                },
-                icon: const Icon(Icons.local_grocery_store_outlined))
-          ],
-        ),
-      ),
+      body: _tabs[_current]['url'],
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.green,
+          iconSize: 30,
+          backgroundColor: const Color.fromARGB(235, 231, 231, 231),
+          onTap: (value) {
+            setState(() {
+              _current = value;
+            });
+          },
+          currentIndex: _current,
+          items: _tabs.map((item) {
+            return BottomNavigationBarItem(
+              icon: item['icon'],
+              label: item['label'],
+            );
+          }).toList()),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, '/photo');
